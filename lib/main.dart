84f40ff99c4a3f072574d5d8083d886621f0a2d8
Angel <darkangel0938@gmail.com>
2021-07-06 19:49:26 +0800

    Create a new object and add lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  int questionNumber = 0;
+
+  List<Question> questionBank = [
+    Question(
+      q: 'You can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +82,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  if (correctAnswer == true) {
+                    print('correct');
+                  } else {
+                    print('wrong');
+                  }
+                  questionNumber++;
+                  scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                });
               },
             ),
           ),
@@ -80,11 +110,23 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  if (correctAnswer == false) {
+                    print('correct');
+                  } else {
+                    print('wrong');
+                  }
+                  questionNumber++;
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
